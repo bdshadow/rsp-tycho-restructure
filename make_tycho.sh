204c13f2fc2e05e60c18ea58319ab013972943fa
Dmitrii Bocharov <dbocharo@redhat.com>
2018-09-13 16:16:01 +0200

    rsp.server.test

--- make_tycho.sh
+++ make_tycho.sh
@@ -19,8 +19,10 @@ git mv ../../launching-java/* ../../launching-java/.[!.]* org.jboss.tools.rsp.la
 git mv ../../server/* ../../server/.[!.]* org.jboss.tools.rsp.server
 git mv ../../server-spi/* ../../server-spi/.[!.]* org.jboss.tools.rsp.server.spi
 cd ../
-mkdir tests/org.jboss.tools.rsp.launching.java.test/src tests/org.jboss.tools.rsp.launching.java.test/resources -p
-cp -rf bundles/org.jboss.tools.rsp.launching.java/src/test/java/* tests/org.jboss.tools.rsp.launching.java.test/src
+mkdir tests/org.jboss.tools.rsp.launching.java.test/src -p
+mv bundles/org.jboss.tools.rsp.launching.java/src/test/java/* tests/org.jboss.tools.rsp.launching.java.test/src
+mkdir tests/org.jboss.tools.rsp.server.test/src -p
+mv bundles/org.jboss.tools.rsp.server/src/test/java/org/jboss/tools/rsp/server/* tests/org.jboss.tools.rsp.server.test/src
 cd ../
 rm -rf launching launching-java server server-spi
 mkdir runtimes/bundles/org.jboss.tools.rsp.server.minishift runtimes/bundles/org.jboss.tools.rsp.server.wildfly -p
