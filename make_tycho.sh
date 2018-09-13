6f3cf8040f14bc8cb2d375155b3178aaea4d457c
Rob Stryker <rob@oxbeef.net>
2018-09-13 16:00:32 -0400

    Fix lol-mkdir bug
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- make_tycho.sh
+++ make_tycho.sh
@@ -2,16 +2,16 @@
 git clone https://github.com/redhat-developer/rsp-server.git
 cd rsp-server
 cd api
-mkdir bundles/org.jboss.tools.rsp.api -p
-mkdir docs/org.jboss.tools.rsp.schema -p
+mkdir -p bundles/org.jboss.tools.rsp.api 
+mkdir -p docs/org.jboss.tools.rsp.schema 
 git mv META-INF src .classpath .gitignore .project build.properties pom.xml bundles/org.jboss.tools.rsp.api
 git mv ../schema/* ../schema/.[!.]* docs/org.jboss.tools.rsp.schema
 cd ../
 rm -rf schema
-mkdir client/jars/org.jboss.tools.rsp.client.cli -p
+mkdir -p client/jars/org.jboss.tools.rsp.client.cli 
 git mv client-cli/* client-cli/.[!.]* client/jars/org.jboss.tools.rsp.client.cli
 rm -rf client-cli
-mkdir framework/bundles -p
+mkdir -p framework/bundles 
 cd framework/bundles
 mkdir org.jboss.tools.rsp.launching org.jboss.tools.rsp.launching.java org.jboss.tools.rsp.server org.jboss.tools.rsp.server.spi
 git mv ../../launching/* ../../launching/.[!.]* org.jboss.tools.rsp.launching
@@ -19,18 +19,18 @@ git mv ../../launching-java/* ../../launching-java/.[!.]* org.jboss.tools.rsp.la
 git mv ../../server/* ../../server/.[!.]* org.jboss.tools.rsp.server
 git mv ../../server-spi/* ../../server-spi/.[!.]* org.jboss.tools.rsp.server.spi
 cd ../
-mkdir tests/org.jboss.tools.rsp.launching.java.test/src -p
+mkdir -p tests/org.jboss.tools.rsp.launching.java.test/src
 mv bundles/org.jboss.tools.rsp.launching.java/src/test/java/* tests/org.jboss.tools.rsp.launching.java.test/src
-mkdir tests/org.jboss.tools.rsp.server.test/src -p
+mkdir -p tests/org.jboss.tools.rsp.server.test/src
 mv bundles/org.jboss.tools.rsp.server/src/test/java/* tests/org.jboss.tools.rsp.server.test/src
 cd ../
 rm -rf launching launching-java server server-spi
-mkdir runtimes/bundles/org.jboss.tools.rsp.server.minishift runtimes/bundles/org.jboss.tools.rsp.server.wildfly -p
+mkdir -p runtimes/bundles/org.jboss.tools.rsp.server.minishift runtimes/bundles/org.jboss.tools.rsp.server.wildfly
 git mv server-minishift/* server-minishift/.[!.]*  runtimes/bundles/org.jboss.tools.rsp.server.minishift
 git mv server-wildfly/* server-wildfly/.[!.]*  runtimes/bundles/org.jboss.tools.rsp.server.wildfly
 rm -rf server-minishift server-wildfly
 cd runtimes
-mkdir tests/org.jboss.tools.rsp.server.wildfly.test/src -p
+mkdir -p tests/org.jboss.tools.rsp.server.wildfly.test/src
 git mv bundles/org.jboss.tools.rsp.server.wildfly/src/test/java/* tests/org.jboss.tools.rsp.server.wildfly.test/src
 git mv bundles/org.jboss.tools.rsp.server.wildfly/src/test/resources/* tests/org.jboss.tools.rsp.server.wildfly.test/
 
